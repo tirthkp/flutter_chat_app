ef03d3e4e2352eeb787ccce135ea942355704e48
Tirth Patel <64749647+tirthkp@users.noreply.github.com>
2024-07-20 15:19:38 +0530

    Update strings.dart

--- lib/config/strings.dart
+++ lib/config/strings.dart
@@ -12,7 +12,7 @@ class WelcomePageString {
 }
 
 class ZegoCloudConfig {
-  static const appId = 786767502;
+  static const appId = [Your Zegocloud appId};
   static const appSign =
-      'ed3e02b4f80f003ff2a917b4e6e9d865986ccab7a34695f2f26bc293b23416ba';
+                        [your zegocloud appSign];
 }
